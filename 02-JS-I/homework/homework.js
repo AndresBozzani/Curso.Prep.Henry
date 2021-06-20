4933d52220e1d90c236f32cadf1c397ed6e51222
Cholo0022 <andres.bozzani@gmail.com>
2021-06-20 14:39:06 -0300

    Parcial homework1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -191,7 +191,6 @@ function esPositivo(numero) {
     return false;
   }
 }
-
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
